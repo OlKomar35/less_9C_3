2b093b92d757ea8a5b60ec1c672b3b4bfa4117ae
Olga Komar <komar_ol@mail.ru>
2023-06-05 17:37:44 +0300

    task 3

--- Program.cs
+++ Program.cs
@@ -2,3 +2,30 @@
 // Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+
+
+Console.Write("Введите m= ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Ackermann(m, n);
+Console.WriteLine($"A({m}, {n}) = {result}");
+
